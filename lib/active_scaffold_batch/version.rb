7187693feed7a738f20536684db7b0cb1567994d
Sergio Cambra <sergio@enpijama.es>
2024-04-05 18:01:05 +0200

    bump to 3.5.0

--- lib/active_scaffold_batch/version.rb
+++ lib/active_scaffold_batch/version.rb
@@ -1,8 +1,8 @@
 module ActiveScaffoldBatch
   module Version
     MAJOR = 3
-    MINOR = 4
-    PATCH = 5
+    MINOR = 5
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
