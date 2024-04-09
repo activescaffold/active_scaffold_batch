7a495497594b35e891233775e404656ea7461294
Sergio Cambra <sergio@enpijama.es>
2024-04-09 16:15:38 +0200

    fix initializer

--- lib/active_scaffold_batch/engine.rb
+++ lib/active_scaffold_batch/engine.rb
@@ -1,21 +1,21 @@
 module ActiveScaffoldBatch
   class Engine < ::Rails::Engine
-    initializer("initialize_active_scaffold_batch", :after => "initialize_active_scaffold") do
-      initializer 'active_scaffold_export.routes' do
-        ActiveSupport.on_load :active_scaffold_routing do
-          self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_edit] = :get
-          self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_update] = :post
-          self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_new] = :get
-          self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_create] = :post
-          self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_add] = :get
-          #not working because routing picks show route instead
-          #self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_destroy] = :get
-          #you may define a route for your controller before resource routes
-          #match 'players/batch_destroy' => 'players#batch_destroy', :via => [:get]
-          self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_destroy] = :delete
-        end
+    initializer 'active_scaffold_batch.routes' do
+      ActiveSupport.on_load :active_scaffold_routing do
+        self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_edit] = :get
+        self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_update] = :post
+        self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_new] = :get
+        self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_create] = :post
+        self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_add] = :get
+        #not working because routing picks show route instead
+        #self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_destroy] = :get
+        #you may define a route for your controller before resource routes
+        #match 'players/batch_destroy' => 'players#batch_destroy', :via => [:get]
+        self::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:batch_destroy] = :delete
       end
+    end
 
+    initializer("active_scaffold_batch.view") do
       ActiveSupport.on_load(:action_view) do
         begin
           include ActiveScaffold::Helpers::UpdateColumnHelpers
