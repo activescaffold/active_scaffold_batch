7813103ee761d4115a057de414d94ad082282a68
Sergio Cambra <sergio@enpijama.es>
2024-12-04 17:27:27 +0100

    fix for AS 4.0, bump to 3.6.0

--- lib/active_scaffold_batch/engine.rb
+++ lib/active_scaffold_batch/engine.rb
@@ -19,11 +19,7 @@ module ActiveScaffoldBatch
       ActiveSupport.on_load(:action_view) do
         begin
           include ActiveScaffold::Helpers::UpdateColumnHelpers
-          if ActiveScaffold.js_framework == :jquery
-            include ActiveScaffold::Helpers::DatepickerUpdateColumnHelpers
-          elsif ActiveScaffold.js_framework == :prototype
-            include ActiveScaffold::Helpers::CalendarDateSelectUpdateColumnHelpers if defined? CalendarDateSelect
-          end
+          include ActiveScaffold::Helpers::DatepickerUpdateColumnHelpers
           include ActiveScaffold::Helpers::BatchCreateColumnHelpers
         rescue
           raise $! unless Rails.env == 'production'
--- lib/active_scaffold_batch/version.rb
+++ lib/active_scaffold_batch/version.rb
@@ -1,8 +1,8 @@
 module ActiveScaffoldBatch
   module Version
     MAJOR = 3
-    MINOR = 5
-    PATCH = 2
+    MINOR = 6
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
