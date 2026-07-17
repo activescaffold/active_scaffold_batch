721438c5cf67ca637589baa6467cfdbe56c5bf25
Sergio Cambra <sergio@enpijama.es>
2026-07-17 12:16:27 +0200

    change version as it will work only with 4.4.0, current master branch of ActiveScaffold

--- lib/active_scaffold_batch/version.rb
+++ lib/active_scaffold_batch/version.rb
@@ -1,8 +1,8 @@
 module ActiveScaffoldBatch
   module Version
     MAJOR = 3
-    MINOR = 7
-    PATCH = 3
+    MINOR = 8
+    PATCH = '0.pre' # release 3.8.0 when AS 4.4.0 is supported, update dependency in gemspec
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
